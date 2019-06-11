ef0d4c78b3897282c884d51d43ad27cf74c5cea9
Ayesh Weerawardena <amweerawardena@gmail.com>
2019-06-11 13:46:26 +1000

    everything 1

---
+++ 6-mudlark/assets/js/main.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+// start here    
+$('.menu-link').click(function() {
+  $('.menu').addClass('left');
+});
+
+$('.close-link').click(function() {
+  $('.menu').removeClass('left');
+});
+
+
+// popup link
+
+
+// popups
+
+// $('.l-1').click(function() {
+//   $('.p-1').addClass('active');
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+// end here
+});
---
+++ 6-mudlark/assets/js/offset-slide.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+
+// right menu    
+$('.nav-contents').click(function() {
+  $('.contents').addClass('left');
+});
+
+$('.close-contents').click(function() {
+  $('.contents').removeClass('left');
+});
+// end right menu
+$('.nav-mudlark').click(function() {
+  $('.mudlark').addClass('left');
+});
+
+$('.close-mudlark').click(function() {
+  $('.mudlark').removeClass('left');
+});
+// left menu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
