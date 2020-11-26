e0508fa9c413c8dfcf8b5efb251e0da7979f9daf
AyeshWeerawardena <48812610+AyeshWeerawardena@users.noreply.github.com>
2020-11-27 01:12:16 +1100

    Edit
    New Edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,432 @@
-// POP UPS LIST
+$(document).ready(function(){
+  console.log('hello world');
 
-$('.r-car').mouseenter(function() {
-    $('.p-car').addClass('p-show');
-  });
+// popup
 
-$('.r-car').mouseleave(function() {
-    $('.p-car').removeClass('p-show');
-  });
+$('.l-1').hover(
+  function(){
+  // first function here
+  $('.i-1').addClass('show');
+}, function(){
+  // second function here
+  $('.i-1').removeClass('show');
+});
 
- $('.r-bike').mouseenter(function() {
-    $('.p-bike').addClass('p-show');
-  });
+$('.l-2').hover(
+  function(){
+  // first function here
+  $('.i-2').addClass('show');
+}, function(){
+  // second function here
+  $('.i-2').removeClass('show');
+});
 
-$('.r-bike').mouseleave(function() {
-    $('.p-bike').removeClass('p-show');
-  });
+$('.l-3').hover(
+  function(){
+  // first function here
+  $('.i-3').addClass('show');
+}, function(){
+  // second function here
+  $('.i-3').removeClass('show');
+});
+
+$('.l-4').hover(
+  function(){
+  // first function here
+  $('.i-4').addClass('show');
+}, function(){
+  // second function here
+  $('.i-4').removeClass('show');
+});
+
+$('.l-5').hover(
+  function(){
+  // first function here
+  $('.i-5').addClass('show');
+}, function(){
+  // second function here
+  $('.i-5').removeClass('show');
+});
+
+$('.l-6').hover(
+  function(){
+  // first function here
+  $('.i-6').addClass('show');
+}, function(){
+  // second function here
+  $('.i-6').removeClass('show');
+});
+
+$('.l-7').hover(
+  function(){
+  // first function here
+  $('.i-7').addClass('show');
+}, function(){
+  // second function here
+  $('.i-7').removeClass('show');
+});
+
+$('.l-8').hover(
+  function(){
+  // first function here
+  $('.i-8').addClass('show');
+}, function(){
+  // second function here
+  $('.i-8').removeClass('show');
+});
+
+$('.l-9').hover(
+  function(){
+  // first function here
+  $('.i-9').addClass('show');
+}, function(){
+  // second function here
+  $('.i-9').removeClass('show');
+});
+
+$('.l-10').hover(
+  function(){
+  // first function here
+  $('.i-10').addClass('show');
+}, function(){
+  // second function here
+  $('.i-10').removeClass('show');
+});
+
+$('.l-11').hover(
+  function(){
+  // first function here
+  $('.i-11').addClass('show');
+}, function(){
+  // second function here
+  $('.i-11').removeClass('show');
+});
+
+$('.l-12').hover(
+  function(){
+  // first function here
+  $('.i-12').addClass('show');
+}, function(){
+  // second function here
+  $('.i-12').removeClass('show');
+});
+
+$('.l-13').hover(
+  function(){
+  // first function here
+  $('.i-13').addClass('show');
+}, function(){
+  // second function here
+  $('.i-13').removeClass('show');
+});
+
+$('.l-14').hover(
+  function(){
+  // first function here
+  $('.i-14').addClass('show');
+}, function(){
+  // second function here
+  $('.i-14').removeClass('show');
+});
+
+$('.l-15').hover(
+  function(){
+  // first function here
+  $('.i-15').addClass('show');
+}, function(){
+  // second function here
+  $('.i-15').removeClass('show');
+});
+
+$('.l-16').hover(
+  function(){
+  // first function here
+  $('.i-16').addClass('show');
+}, function(){
+  // second function here
+  $('.i-16').removeClass('show');
+});
+
+$('.l-17').hover(
+  function(){
+  // first function here
+  $('.i-17').addClass('show');
+}, function(){
+  // second function here
+  $('.i-17').removeClass('show');
+});
+
+$('.l-18').hover(
+  function(){
+  // first function here
+  $('.i-18').addClass('show');
+}, function(){
+  // second function here
+  $('.i-18').removeClass('show');
+});
+
+$('.l-19').hover(
+  function(){
+  // first function here
+  $('.i-19').addClass('show');
+}, function(){
+  // second function here
+  $('.i-19').removeClass('show');
+});
+
+$('.l-20').hover(
+  function(){
+  // first function here
+  $('.i-20').addClass('show');
+}, function(){
+  // second function here
+  $('.i-20').removeClass('show');
+});
+
+$('.l-21').hover(
+  function(){
+  // first function here
+  $('.i-21').addClass('show');
+}, function(){
+  // second function here
+  $('.i-21').removeClass('show');
+});
+
+$('.l-22').hover(
+  function(){
+  // first function here
+  $('.i-22').addClass('show');
+}, function(){
+  // second function here
+  $('.i-22').removeClass('show');
+});
+
+$('.l-23').hover(
+  function(){
+  // first function here
+  $('.i-23').addClass('show');
+}, function(){
+  // second function here
+  $('.i-23').removeClass('show');
+});
+
+$('.l-24').hover(
+  function(){
+  // first function here
+  $('.i-24').addClass('show');
+}, function(){
+  // second function here
+  $('.i-24').removeClass('show');
+});
+
+$('.l-25').hover(
+  function(){
+  // first function here
+  $('.i-25').addClass('show');
+}, function(){
+  // second function here
+  $('.i-25').removeClass('show');
+});
+
+$('.l-26').hover(
+  function(){
+  // first function here
+  $('.i-26').addClass('show');
+}, function(){
+  // second function here
+  $('.i-26').removeClass('show');
+});
+
+$('.l-27').hover(
+  function(){
+  // first function here
+  $('.i-27').addClass('show');
+}, function(){
+  // second function here
+  $('.i-27').removeClass('show');
+});
+
+$('.l-28').hover(
+  function(){
+  // first function here
+  $('.i-28').addClass('show');
+}, function(){
+  // second function here
+  $('.i-28').removeClass('show');
+});
+
+$('.l-29').hover(
+  function(){
+  // first function here
+  $('.i-29').addClass('show');
+}, function(){
+  // second function here
+  $('.i-29').removeClass('show');
+});
+
+$('.l-30').hover(
+  function(){
+  // first function here
+  $('.i-30').addClass('show');
+}, function(){
+  // second function here
+  $('.i-30').removeClass('show');
+});
 
 
 
- $('.nav-contents').mouseenter(function() {
-    $('.contents').addClass('top');
-  });
 
 
- $('.nav-contents').mouseleave(function() {
-    $('.contents').removeClass('top');
-  });
+
+
+//console.log('btn camera is hovered');//
+
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+
+//     // Life From New Worlds - button and hover
+
+//     $('.btn-lfnw').mouseenter(function(){
+//         console.log('btn camera is hovered');
+//         $('.r-cam').addClass('show');
+//     });
+
+//     $('.btn-lfnw').mouseleave(function(){
+//         console.log('btn camera has left');
+//         $('.r-cam').removeClass('show');
+//     });
+
+//    // excel - button and hover
+
+//     $('.btn-excel').mouseenter(function(){
+//         console.log('btn excel is hovered');
+//         $('.r-excel').addClass('show');
+//     });
+
+//     $('.btn-excel').mouseleave(function(){
+//         console.log('btn excel has left');
+//         $('.r-excel').removeClass('show');
+//     });
+
+//       // p2p - button and hover
+
+//     $('.btn-p2p').mouseenter(function(){
+//         console.log('btn p2p is hovered');
+//         $('.r-p2p').addClass('show');
+//     });
+
+//     $('.btn-p2p').mouseleave(function(){
+//         console.log('btn p2p has left');
+//         $('.r-p2p').removeClass('show');
+//     });
+     
+//            // gmap - button and hover
+
+//     $('.btn-gmap').mouseenter(function(){
+//         console.log('btn gmap is hovered');
+//         $('.r-gmap').addClass('show');
+//     });
+
+//     $('.btn-gmap').mouseleave(function(){
+//         console.log('btn gmap has left');
+//         $('.r-gmap').removeClass('show');
+//     });
+
+//            // deluxe - button and hover
+
+//     $('.btn-deluxe').mouseenter(function(){
+//         console.log('btn deluxe is hovered');
+//         $('.r-deluxe').addClass('show');
+//     });
+
+//     $('.btn-deluxe').mouseleave(function(){
+//         console.log('btn deluxe has left');
+//         $('.r-deluxe').removeClass('show');
+//     });
+
+//         // audacity - button and hover
+     
+//      $('.btn-audacity').mouseenter(function(){
+//         console.log('btn audacity is hovered');
+//         $('.r-audacity').addClass('show');
+//     });
+
+//     $('.btn-audacity').mouseleave(function(){
+//         console.log('btn audacity has left');
+//         $('.r-audacity').removeClass('show');
+//     });
+     
+//          // fax - button and hover
+     
+//      $('.btn-fax').mouseenter(function(){
+//         console.log('btn fax is hovered');
+//         $('.r-fax').addClass('show');
+//     });
+
+//     $('.btn-fax').mouseleave(function(){
+//         console.log('btn fax has left');
+//         $('.r-fax').removeClass('show');
+//     });
+     
+//         // bomomo - button and hover
+     
+//      $('.btn-bom').mouseenter(function(){
+//         console.log('btn bom is hovered');
+//         $('.r-bom').addClass('show');
+//     });
+
+//     $('.btn-bom').mouseleave(function(){
+//         console.log('bom has left');
+//         $('.r-bom').removeClass('show');
+//     });
+
+//   // camera fall - button and hover
+     
+//      $('.btn-fall').mouseenter(function(){
+//         console.log('btn fall is hovered');
+//         $('.r-fall').addClass('show');
+//     });
+
+//     $('.btn-fall').mouseleave(function(){
+//         console.log('fall has left');
+//         $('.r-fall').removeClass('show');
+//     });
+
+//     // blender - button and hover
+     
+//      $('.btn-blender').mouseenter(function(){
+//         console.log('btn blender is hovered');
+//         $('.r-blender').addClass('show');
+//     });
+
+//     $('.btn-blender').mouseleave(function(){
+//         console.log('blender has left');
+//         $('.r-blender').removeClass('show');
+//     });
+
+//     // dots - button and hover
+     
+//      $('.btn-dots').mouseenter(function(){
+//         console.log('btn dots is hovered');
+//         $('.r-dots').addClass('show');
+//     });
+
+//     $('.btn-dots').mouseleave(function(){
+//         console.log('dots has left');
+//         $('.r-dots').removeClass('show');
+//     });
+
+//         // slitscan - button and hover
+     
+//      $('.btn-slit').mouseenter(function(){
+//         console.log('btn slit is hovered');
+//         $('.r-slit').addClass('show');
+//     });
+
+//     $('.btn-slit').mouseleave(function(){
+//         console.log('slit has left');
+//         $('.r-slit').removeClass('show');
+//     });
+
+
+// });
